684e166c114ef5847b3708dcac68077403724b23
Danielle Voznyy <dan.voznyy@gmail.com>
2025-07-28 21:44:21 +0200

    feat: Add publish workflow with new version calculation

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,30 +54,3 @@ publishing {
         }
     }
 }
-
-// COPIED FROM AUTOVERSION, we can't apply the plugin to this project
-fun getNextVersion(): String {
-    val releaseVersion: String? = System.getenv("RELEASE_VERSION")
-    val branchVersion: String? = System.getenv("BRANCH_VERSION")
-    val isSnapshot = System.getenv("IS_SNAPSHOT") == "true"
-
-    if (releaseVersion == null) return "$version-dev"
-
-    val (major, minor) = version.toString().split(".").takeIf { it.size >= 2 }
-        ?: error("Version $version does not match major.minor format!")
-    fun bump(bump: String?, matchPatch: String? = null) = bump
-        ?.removePrefix("v")
-        ?.replace(Regex("-\\w*"), "")
-        ?.split(".")
-        ?.takeIf { it.size > 2 && it[0] == major && it[1] == minor && (matchPatch == null || it.getOrNull(2) == matchPatch) }
-        ?.lastOrNull()?.toInt()?.plus(1)
-        ?: 0
-
-    return buildString {
-        val bumpedPatch = bump(releaseVersion)
-        append("$major.$minor.$bumpedPatch")
-        if (isSnapshot) append("-dev.${bump(branchVersion, bumpedPatch.toString())}")
-    }
-}
-
-version = getNextVersion()
--- src/main/kotlin/com.mineinabyss.conventions.autoversion.gradle.kts
+++ src/main/kotlin/com.mineinabyss.conventions.autoversion.gradle.kts
@@ -1,36 +1,6 @@
 import java.net.URL
 import java.util.*
 
-fun getNextVersion(): String {
-    val releaseVersion: String? = System.getenv("RELEASE_VERSION")
-    val branchVersion: String? = System.getenv("BRANCH_VERSION")
-    val isSnapshot = System.getenv("IS_SNAPSHOT") == "true"
-
-    if (releaseVersion == null) return "$version-dev"
-
-    val (major, minor) = version.toString().split(".").takeIf { it.size >= 2 }
-        ?: error("Version $version does not match major.minor format!")
-    fun bump(bump: String?, matchPatch: String? = null) = bump
-        ?.removePrefix("v")
-        ?.replace(Regex("-\\w*"), "")
-        ?.split(".")
-        ?.takeIf { it.size > 2 && it[0] == major && it[1] == minor && (matchPatch == null || it.getOrNull(2) == matchPatch) }
-        ?.lastOrNull()?.toInt()?.plus(1)
-        ?: 0
-
-    return buildString {
-        val bumpedPatch = bump(releaseVersion)
-        append("$major.$minor.$bumpedPatch")
-        if (isSnapshot) append("-dev.${bump(branchVersion, bumpedPatch.toString())}")
-    }
-}
-
-version = getNextVersion()
-
-subprojects {
-    version = rootProject.version
-}
-
 tasks.register("updateIdofrontVersion") {
     doLast {
         val githubApiUrl = "https://api.github.com/repos/MineInAbyss/Idofront/releases/latest"
